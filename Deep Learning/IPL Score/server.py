7ec5658374a34c8c7dc92f4fa7394941aa6e7d26
vxshnu <vishnunarayanan8690@gmail.com>
2024-05-30 00:47:46 +0530

    a

--- Deep Learning/IPL Score/server.py
+++ Deep Learning/IPL Score/server.py
@@ -1,8 +1,10 @@
-from flask import Flask,render_template,redirect,url_for,request
+from flask import Flask,render_template,redirect,url_for,request,jsonify
 import pandas as pd
+import json
+import requests
 app=Flask(__name__)
 
-data=pd.read_csv('ipl_data.csv')
+data=pd.read_csv('Deep Learning/IPL Score/ipl_data.csv')
 
 @app.route('/')
 def home():
@@ -11,10 +13,20 @@ def home():
 @app.route('/predicted',methods=['POST'])
 def predict():
     venue=request.form.get('venue')
+    print("Venu:",venue)
     batting_team=request.form.get('batting_team')
     bowling_team=request.form.get('bowling_team')
     striker=request.form.get('striker')
     bowler=request.form.get('bowler')
-    
+    data={'venue':venue,'batting':batting_team,'bowling':bowling_team,'striker':striker,'bowler':bowler}
+    #json_data=json.dumps(data)
+    json_data=jsonify(data)
+    print(json_data)
+    url='http://localhost:8888/receive_data'
+    #headers = {'Content-Type': 'application/json'}
+    response=requests.post(url,data=data)
+    print("response:",response)
+    result=response.json()
+    return render_template('score.html',team1=batting_team,team2=bowling_team,score=result['score'])
     
 app.run(debug=True)
